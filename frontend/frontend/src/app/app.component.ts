ec4addb52550a529e4567ed90c351ebc7773161c
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-07 20:55:52 -0300

    Creating a nav component

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/template/header/header.component';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, NavComponent, RouterOutlet,MatListModule, MatSidenavModule],
   templateUrl: 'app.component.html',
+
 })
 export class AppComponent {
   nome = 'samuca';
