e9b7cd1fe16b24d9e4db7b5730ee65fa12da0435
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-07 18:38:13 -0300

    Making a header

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/template/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [HeaderComponent],
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
