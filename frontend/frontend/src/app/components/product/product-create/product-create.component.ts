ad34867b98d70c7876c654463e5b460b5181f21d
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-08 17:36:00 -0300

    Making logic for create a product

--- frontend/frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,22 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
+import {MatButtonModule} from '@angular/material/button'
+import { Router } from '@angular/router';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-product-create',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  styleUrl: './product-create.component.css',
+  providers: []
 })
 export class ProductCreateComponent implements OnInit {
+  product: Product = {
+    name: 'Computer',
+    price: 12000,
+  }
 
-  constructor (private productService: ProductService) {}
+  constructor (private productService: ProductService, private router:Router) {}
 
   ngOnInit(): void {
 
   }
 
   createProduct(): void {
+
+    this.productService.create(this.product)
     this.productService.showMessage('Operação executada com sucesso')
+  }
+
+  cancel(): void {
+
+    this.router.navigate(['/products'])
   }
 }
