5f3095cd6ba3b9c56b652d78eb4e2ca673bbc693
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-10 20:48:41 -0300

    Component read

--- frontend/frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/frontend/src/app/components/product/product-create/product-create.component.ts
@@ -23,7 +23,7 @@ export class ProductCreateComponent implements OnInit {
   product: Product = {
     name: '',
     price: 0
-  }
+    }
 
   constructor (private productService: ProductService, private router:Router) {}
 
--- frontend/frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -13,6 +13,7 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductReadComponent implements OnInit{
   products!: Product[];
+  displayedColumns = ['id', 'name', 'price']
   
   constructor (private productService: ProductService) {
   }
@@ -20,7 +21,6 @@ export class ProductReadComponent implements OnInit{
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-      console.log(products)
     })
   }
 }
