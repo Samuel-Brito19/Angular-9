72de8e7fb90788d6f3776d8f84fde586db1a42eb
Samuel-Brito19 <sambriton7@gmail.com>
2024-06-10 21:06:04 -0300

    Customazing the table

--- frontend/frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, LOCALE_ID } from '@angular/core';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table'
+
+import localePt from '@angular/common/locales/pt'
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt)
+
 
 
 @Component({
   selector: 'app-product-read',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTableModule],
   templateUrl: './product-read.component.html',
-  styleUrl: './product-read.component.css'
+  styleUrl: './product-read.component.css',
+  providers: [{
+    provide: LOCALE_ID,
+    useValue: 'pt-BR'
+  }]
 })
 export class ProductReadComponent implements OnInit{
   products!: Product[];
